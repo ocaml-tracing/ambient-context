78fb12116950d56dcc0d9c0eb997e082dd867c33
Simon Cruanes <simon.cruanes.2007@m4x.org>
2026-01-15 21:47:38 -0500

    update tests

--- test/unix/test_basics.ml
+++ test/unix/test_basics.ml
@@ -2,30 +2,27 @@ open Alcotest
 module Ctx = Ambient_context
 
 let test_create_key () =
-   let _k = Ctx.create_key () in
-   ()
-
+  let _k = Ctx.new_key () in
+  ()
 
 let test_set_then_get () =
-   let k = Ctx.create_key () in
-   Ctx.with_binding k "hello" @@ fun () ->
-   let v = Ctx.get k in
-   check (option string) "retrieve same string" v (Some "hello")
-
+  let k = Ctx.new_key () in
+  Ctx.with_key_bound_to k "hello" @@ fun () ->
+  let v = Ctx.get k in
+  check (option string) "retrieve same string" v (Some "hello")
 
 let test_set_then_unset () =
-   let k = Ctx.create_key () in
-   Ctx.with_binding k "hello" @@ fun () ->
-   let v = Ctx.get k in
-   check (option string) "retrieve same string" v (Some "hello") ;
-   Ctx.without_binding k @@ fun () ->
-   let v = Ctx.get k in
-   check (option string) "retrieve nothing" v None
-
+  let k = Ctx.new_key () in
+  Ctx.with_key_bound_to k "hello" @@ fun () ->
+  let v = Ctx.get k in
+  check (option string) "retrieve same string" v (Some "hello");
+  Ctx.with_key_unbound k @@ fun () ->
+  let v = Ctx.get k in
+  check (option string) "retrieve nothing" v None
 
 let suite =
-   [
-     ("can create keys", `Quick, test_create_key);
-     ("can set, and get, keys", `Quick, test_set_then_get);
-     ("can unset keys", `Quick, test_set_then_unset);
-   ]
+  [
+    "can create keys", `Quick, test_create_key;
+    "can set, and get, keys", `Quick, test_set_then_get;
+    "can unset keys", `Quick, test_set_then_unset;
+  ]
